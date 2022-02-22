6edf9a31a559959f4a80de9a57af85fa17690ff2
Reeshoon <reeshoon@gmail.com>
2022-02-23 04:13:12 +0600

    Fixed an error

--- dataloader.py
+++ dataloader.py
@@ -117,6 +117,11 @@ class SHRECLoader(data.Dataset):
 
             elif shift > 0: # cut off some end frames
                 image_sequence = image_sequence[:-shift]
+            
+            num_frames = image_sequence.shape[0]
+        
+            if num_frames < 32:
+                image_sequence = np.pad(image_sequence, ((0, self.T - num_frames), (0, 0), (0, 0), (0, 0)), mode="constant")
 
         return image_sequence
 
