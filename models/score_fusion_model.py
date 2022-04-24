cd294e31f8f87289b4d4410865e8c7dc65445cb3
reeshoon <reeshoon@gmail.com>
2022-04-24 21:46:09 +0600

    pointlstm only

--- models/score_fusion_model.py
+++ models/score_fusion_model.py
@@ -20,26 +20,26 @@ class PointDepthScoreFusion(nn.Module):
             knn=[16, 24, 48, 12]
         )
 
-        self.depth_crnn_model=DepthCRNN(
-            num_classes=14,
-            conv_blocks=[8, 16, 32],
-            res_in=[50, 50],
-            T=32,
-            mlp_layers=[128],
-            drop_prb=0.5,
-            lstm_units=128,
-            lstm_layers=2,
-            use_bilstm=True,
-            use_bn=True,
-            actn_type="relu"
-        )
+        # self.depth_crnn_model=DepthCRNN(
+        #     num_classes=14,
+        #     conv_blocks=[8, 16, 32],
+        #     res_in=[50, 50],
+        #     T=32,
+        #     mlp_layers=[128],
+        #     drop_prb=0.5,
+        #     lstm_units=128,
+        #     lstm_layers=2,
+        #     use_bilstm=True,
+        #     use_bn=True,
+        #     actn_type="relu"
+        # )
 
     def forward(self, x_ptcloud, x_depth):
 
         x_ptcloud = self.point_lstm_model(x_ptcloud)
-        x_depth = self.depth_crnn_model(x_depth)
+        #x_depth = self.depth_crnn_model(x_depth)
 
         #x_fused = (x_ptcloud + x_depth) / 2
-        x_fused = torch.max(x_ptcloud,x_depth)
-        return x_fused
+        #x_fused = torch.max(x_ptcloud,x_depth)
+        return x_ptcloud
 
